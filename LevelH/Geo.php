ab7e9f3f4dca2fb042bad0f916ac58b896230faa
francois.bibron <francois.bibron@epita.fr>
2022-10-26 21:24:02 +0200

    levelH
    Signed-off-by: francois.bibron <francois.bibron@epita.fr>

--- LevelH/Geo.php
+++ LevelH/Geo.php
@@ -52,20 +52,9 @@ class Geo
      */
 
     private function computeDistance($lat1, $lng1, $lat2, $lng2, $cosDeg2RadLat1){
-
-        $earth_radius = 6378137; // Earth Radius is 6378.137 km
-        $rlo1 = deg2rad($lng1);
-        $rla1 = deg2rad($lat1);
-        $rlo2 = deg2rad($lng2);
-        $rla2 = deg2rad($lat2);
-        $dlo = ($rlo2 - $rlo1) / 2;
-        $dla = ($rla2 - $rla1) / 2;
-        $a =
-            (sin($dla) * sin($dla)) +
-            cos($rla1) * cos($rla2) *
-            (sin($dlo) * sin($dlo));
-        $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
-
-        return ($earth_radius * $d) / pow(10, 3);
+        return acos(
+            $cosDeg2RadLat1 * cos(deg2rad($lat2)) * cos(deg2rad($lng2) - deg2rad($lng1)) +
+            sin(deg2rad($lat1)) * sin(deg2rad($lat2))
+        ) * 6371 * 1000;
     }
 };
