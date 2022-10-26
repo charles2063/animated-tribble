7e0fef04f645828cd4c7b5e8b47ddba79270ca99
MateosGit <guillaume.mateos@hotmail.fr>
2022-10-26 21:27:01 +0200

    [ADD] Level K

--- LevelK/Brute.php
+++ LevelK/Brute.php
@@ -14,14 +14,23 @@ class Brute
     }
 
     /**
-     * @TODO :
+     * @TODO : 97 -> 122
      *
      * Cette méthode essaye de trouver par la force à quel mot de 4 lettres correspond ce hash.
      * Sachant que nous ne connaissons pas le hash (enfin si... il suffit de regarder les commentaires de l'attribut privé $methode.
      */
     public function force()
     {
+        $val = "aaaa";
 
-        // @TODO
+        while ($val != "aaaaa") {
+
+            if (md5($val) == $this->hash || crc32($val) == $this->hash || base64_encode($val) == $this->hash || sha1($val) == $this->hash) {
+                $this->origin = $val;
+                return;
+            }
+
+            $val++;
+        }
     }
 }
